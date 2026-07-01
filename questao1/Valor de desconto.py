ad88537ca5bddab97c90751c4b6d077a34f76730
RodrigoJordao97 <rodrigojordao97@hotmail.com>
2026-07-01 20:49:27 -0300

    nome alterado

--- questao1/Valor de desconto.py
+++
@@ -1,5 +0,0 @@
-def calcular_desconto(total_bruto, valor_pago):
-    if total_bruto == 0:
-        return 0.0
-    percentual_desc = ((total_bruto - valor_pago) / total_bruto) * 100
-    return percentual_desc
